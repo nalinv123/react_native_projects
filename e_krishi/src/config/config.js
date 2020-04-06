bcacec50a0f60d5b7aef138994740cce37a20825
Nalin Vaidya <nalinvaidya@gmail.com>
2020-04-06 12:55:30 +0700

    Removed the e_krishi project from this repo and added to the gitignore file.

--- e_krishi/src/config/config.js
+++
@@ -1,5 +0,0 @@
-export const delearBaseUrl = "http://localhost:8080/e_krishi/v1/dealer";
-export const loginUrl = "/authenticate";
-export const registerUrl = "/register";
-export const getDealerVegetablesUrl = "/get_dealer_vegetables";
-export const logoutUrl = "/logout";
